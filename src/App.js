0b5143c262669e2633523f7a95f05aff06c8be81
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-12-25 00:17:11 +0600

    setup complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-         <h2 className="text-5xl text-center">Welcome to Bye Mea Coffee</h2>
+         <h2 className="text-5xl text-center text-rose-500">Welcome to Buy Mea Coffee</h2>
     </div>
   );
 }
