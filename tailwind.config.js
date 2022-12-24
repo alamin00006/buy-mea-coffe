28f7b1bb4a02d7a5b099c99d6cbc70286c2feb0a
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-12-25 00:00:54 +0600

    basic setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
