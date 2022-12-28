acfec87da6c1c82ae6ea6d5540db0be8eff6a7ea
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-12-28 17:29:15 +0600

    side Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import DashBoardHome from "./components/Home/SideNavbar/DashBoardHome";
 
 
 function App() {
   return (
     <div>
         <Home></Home>
+        <Routes>
+           <Route path="/dashboardHome" element={<DashBoardHome></DashBoardHome>}></Route>
+        </Routes>
     </div>
   );
 }
